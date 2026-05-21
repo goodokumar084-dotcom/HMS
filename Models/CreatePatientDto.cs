6b68cb94e2ecd82e55ad66b262e6aa11d1db0263
Goodo Kumar <goodokumar084@gmail.com>
2026-05-21 12:32:16 +0530

    Models

---
+++ Models/CreatePatientDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HospitalManagementApi.Models
+{
+	public class CreatePatientDto
+	{
+		public string? FullName { get; set; }
+		public string? Email { get; set; }
+		public string? Phone { get; set; }
+		public DateOnly DateOfBirth { get; set; }
+	}
+}
